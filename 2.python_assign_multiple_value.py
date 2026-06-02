dc7563ecda4306517a8297fb71066202d146f4ff
khairul islam <khairulsagor567@gmail.com>
2026-06-02 22:47:05 +0600

    comments added in this page 2.python_assign_multiple_value.py

--- 2.python_assign_multiple_value.py
+++ 2.python_assign_multiple_value.py
@@ -1,15 +1,19 @@
+#Many Values to Multiple Variables:
+
 x, y, z = "Orange", "Banana", "Cherry"
 print(x)
 print(y)
 print(z)
 
-#One Value to Multiple Variables
+#One Value to Multiple Variables:
+
 x = y = z = "Orange"
 print(x)
 print(y)
 print(z)
 
-#Unpack a Collection
+#Unpack a Collection:
+
 fruits = ["Apple", "Banana", "Cherry"]  
 x, y, z = fruits
 print(x)
