e51f148a84c0bd18aef74711d862ebff3b5e2b2d
khairul islam <khairulsagor567@gmail.com>
2026-06-02 17:37:36 +0600

    one value to multiple variable and unpack a collection

--- 2.python_assign_multiple_value.py
+++ 2.python_assign_multiple_value.py
@@ -2,3 +2,16 @@ x, y, z = "Orange", "Banana", "Cherry"
 print(x)
 print(y)
 print(z)
+
+#One Value to Multiple Variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+#Unpack a Collection
+fruits = ["Apple", "Banana", "Cherry"]  
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
