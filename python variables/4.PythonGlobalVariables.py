2c626ae57a6f3b57ed8dd560631ccd4049bf0677
khairul islam <khairulsagor567@gmail.com>
2026-06-03 00:05:45 +0600

    added a sample for local and global variable

--- python variables/4.PythonGlobalVariables.py
+++ python variables/4.PythonGlobalVariables.py
@@ -12,11 +12,22 @@ def sum():
 x =15
 
 def sum () :
-    global x # this will tell Python that we want to use the global variable x, instead of creating a local variable with the same name.
+    global x # this will tell Python that we want to use the global variable x, instead of creating a local variable with the same name.      
     print(x + 5)
     
     x=10 # this is a local variable, because it is inside a function, and can only be used inside that function.
 
-sum() # this will print 15, because the local variable x is used inside the function, and it has a value of 10, so 10 + 5 = 15.
+#sum() # this will print 15, because the local variable x is used inside the function, and it has a value of 10, so 10 + 5 = 15.
 
-print(x) # this will print 10, because the global variable x is used outside the function, and it has a value of 10.
+#print(x) # this will print 10, because the global variable x is used outside the function, and it has a value of 10.
+
+
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x) 
