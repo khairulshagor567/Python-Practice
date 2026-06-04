496b59668c6591ec59c978386a07dbf0ebdc8a76
khairul islam <khairulsagor567@gmail.com>
2026-06-05 00:08:24 +0600

    creates a python strings directory and python strings file is also create.

---
+++ python string/1.strings_are_arrays.py
@@ -0,0 +1,20 @@
+
+#Strings are Arrays:
+
+a = "Hello, World!"
+#print(a[1])
+
+#Looping Through a String:
+
+fruit = "banana"
+for x in fruit:
+    print(x)
+
+
+#String Length:
+
+greetings = "Hello, World!"
+
+#print(len(greetings))
+
+
