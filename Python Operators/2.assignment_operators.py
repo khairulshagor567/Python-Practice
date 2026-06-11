beb52aed5570d73edb37c0d8834ebd04321f5aed
khairul islam <khairulsagor567@gmail.com>
2026-06-11 22:51:18 +0600

    cerate python operators folder and it has arithmetic and assignment operators file

---
+++ Python Operators/2.assignment_operators.py
@@ -0,0 +1,20 @@
+#Assignment Operators
+
+x = 10
+x += 5  # Equivalent to x = x + 5
+print(x)  # Output: 15
+
+x -= 3  # Equivalent to x = x - 3
+print(x)  # Output: 12
+
+x *= 2  # Equivalent to x = x * 2
+print(x)  # Output: 24
+
+x /= 4  # Equivalent to x = x / 4
+print(x)  # Output: 6.0
+
+
+#The Walrus Operator
+
+if(n := len("khairul"))>5:
+    print(f"The length of the string is {n}")
