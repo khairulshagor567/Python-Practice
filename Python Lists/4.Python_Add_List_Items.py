2b894318535fc8587288f74f781f5578cbaa270e
khairul islam <khairulsagor567@gmail.com>
2026-06-14 18:03:39 +0600

    added items in a list file

---
+++ Python Lists/4.Python_Add_List_Items.py
@@ -0,0 +1,13 @@
+#Append Items
+
+#To add an item to the end of the list, use the append() method:
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+# print(thislist)
+
+
+#Insert Items
+
+thisList = ["apple", "banana", "cherry"]
+thisList.insert(1,"watermelon") #The insert() method inserts an item at the specified index, so the new item will be inserted before the specified index.
+print(thisList)
