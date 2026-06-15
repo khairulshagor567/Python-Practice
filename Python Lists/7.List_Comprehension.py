9cb18eb6f593a9cb0043f6a78b4aaf92cd60a3a5
khairul islam <khairulsagor567@gmail.com>
2026-06-15 17:22:40 +0600

    added some code in list comprehensions and python sort lists

--- Python Lists/7.List_Comprehension.py
+++ Python Lists/7.List_Comprehension.py
@@ -19,6 +19,20 @@ fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 
 #[print(x) for x in fruits if "a" in x]
 
-newList = [x for x in fruits if "a" in x]
+# newList = [x for x in fruits if "a" in x]
+# print(newList)
+
+# newList = [x for x in fruits if x!="apple"]
+# print(newList)
+
+# newList = [x for x in fruits]
+# print(newList)
+
+newlistNumbers = [x for x in range(10)]
+print(newlistNumbers)
+
+newList = [x if x!="banana" else "orange" for x in fruits]
 print(newList)
+
+
 
