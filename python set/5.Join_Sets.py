9fd90e0909c2d7bcf3e2104b57918700bb75497a
khairul islam <khairulsagor567@gmail.com>
2026-06-17 23:21:50 +0600

    intersection_update and difference method for set joins

--- python set/5.Join_Sets.py
+++ python set/5.Join_Sets.py
@@ -42,5 +42,35 @@ set2 = {"google", "microsoft", "apple"}
 # set3 = set1.intersection(set2) 
 set3 = set1 & set2 #this is another way for intersection
 
+# print(set3)
+
+#intersection update (Keep ONLY the duplicates)
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.intersection_update(set2) #intersection_update() method will also keep ONLY the duplicates, but it will change the original set instead of returning a new set.
+# print(set1)
+
+#The values True and 1 are considered the same value. The same goes for False and 0.
+
+set1 = {"apple", 1,  "banana", 0, "cherry"}
+set2 = {False, "google", 1, "apple", 2, True}
+
+set3 = set1.intersection(set2)
+# print(set3)
+
+#Difference
+
+#The difference() method will return a new set that will contain only the items from the first set that are not present in the other set.
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+#it gives only the items from the first set that are no present in the other set.
+
+#set3 = set1.difference(set2) 
+
+set3 = set1 - set2 
 print(set3)
 
