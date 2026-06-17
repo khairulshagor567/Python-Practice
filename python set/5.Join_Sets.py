cac5c3c17e21d83a9d657538b5374066259f92e5
khairul islam <khairulsagor567@gmail.com>
2026-06-17 23:24:43 +0600

    intersection_update and difference method for set joins

--- python set/5.Join_Sets.py
+++ python set/5.Join_Sets.py
@@ -64,7 +64,7 @@ set3 = set1.intersection(set2)
 
 #The difference() method will return a new set that will contain only the items from the first set that are not present in the other set.
 
-set1 = {"apple", "banana", "cherry"}
+set1 = {"apple", "banana", "cherry"} 
 set2 = {"google", "microsoft", "apple"}
 
 #it gives only the items from the first set that are no present in the other set.
