89470773a02d0d6f43b80d9e8bab456717bd63cd
khairul islam <khairulsagor567@gmail.com>
2026-06-17 23:29:07 +0600

    set difference update and symmetric difference

--- python set/5.Join_Sets.py
+++ python set/5.Join_Sets.py
@@ -72,5 +72,21 @@ set2 = {"google", "microsoft", "apple"}
 #set3 = set1.difference(set2) 
 
 set3 = set1 - set2 
+# print(set3)
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.difference_update(set2)
+
+# print(set1)
+
+#Symmetric Differences
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.symmetric_difference(set2)
+
 print(set3)
 
