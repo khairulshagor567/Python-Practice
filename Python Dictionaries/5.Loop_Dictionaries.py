0960eecc35f70b49170b2da94fbced5eccfa932a
khairul islam <khairulsagor567@gmail.com>
2026-06-25 14:37:12 +0600

    added some methods and loop dictionary for python

---
+++ Python Dictionaries/5.Loop_Dictionaries.py
@@ -0,0 +1,23 @@
+#Loop Through a Dictionary
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "wheels":4,
+  "color":"White",
+  "seats":5
+}
+
+# for x in thisdict.keys():
+#    #print(x) Print all key names in the dictionary, one by one: 
+#    #print(thisdict[x])Print all values in the dictionary, one by one:
+#    print(x)
+
+# for x in thisdict.values():
+#    #print(x) Print all key names in the dictionary, one by one: 
+#    #print(thisdict[x])Print all values in the dictionary, one by one:
+#    print(x)   
+
+# for x,y in thisdict.items():
+#    print(x,y)  
