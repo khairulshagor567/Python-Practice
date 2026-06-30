d43f3c352cd283c3e108048e0eec09cd034b5fe9
khairul islam <khairulsagor567@gmail.com>
2026-06-30 22:43:23 +0600

    python if and elif condtion practice

--- Python If Statement/1.Python_If_Statement.py
+++ Python If Statement/1.Python_If_Statement.py
@@ -1,5 +1,12 @@
 #Python If Statement
 
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+
 a = 33
 b = 200
 
