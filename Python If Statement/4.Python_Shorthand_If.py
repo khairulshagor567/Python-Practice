8aac2fa71d6f3b6ce101ffdbe96cd5edbd5874c5
khairul islam <khairulsagor567@gmail.com>
2026-07-01 23:50:52 +0600

    python shorthand if add

---
+++ Python If Statement/4.Python_Shorthand_If.py
@@ -0,0 +1,18 @@
+#Short Hand If
+
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+
+#Short Hand If ... Else
+
+a = 2
+b = 330
+print("A") if a > b else print("B") #This is called a conditional expression (sometimes known as a "ternary operator").
+
+#Assign a Value With If ... Else
+
+a = 10
+b = 20
+bigger = a if a > b else b
+print("Bigger is", bigger)
